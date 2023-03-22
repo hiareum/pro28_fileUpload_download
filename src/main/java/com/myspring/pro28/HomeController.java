91cd352355f51c886d3b2d6040a5a7ef691a8b6c
hiareum <j_ar2979@naver.com>
2023-03-22 12:58:51 +0900

    230322 home컨트롤러 주석처리 및 로컬컨트롤러 실행해서 다국어기능 확인

--- src/main/java/com/myspring/pro28/HomeController.java
+++ src/main/java/com/myspring/pro28/HomeController.java
@@ -11,10 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
-/* @Controller */
+
+// @Controller 
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
